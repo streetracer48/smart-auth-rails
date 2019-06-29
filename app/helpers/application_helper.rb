fb13505fbf40e9aa1ab5181b83f2b0f1df893bdb
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-29 21:44:12 +0600

    avatar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def gravatar_for(user, options = {size:80})
+       
+      gravatar_id= Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    end
 end
