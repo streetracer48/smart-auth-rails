b15afd628caf21eaeb4ba4369984a842f09f5150
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-30 14:53:26 +0600

    application helper method updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
 
     def gravatar_for(user, options = {size:80})
-       
+
+     if user.image 
+      "http://graph.facebook.com/#{user.uid}/picture?type=large"
+     else
       gravatar_id= Digest::MD5::hexdigest(user.email.downcase)
       size = options[:size]
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     end
+  end
 end
